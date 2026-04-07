831f9304da1e5e66e635486363a96ac6daebd6bf
AswatheeSathish <aswatheesbca25@rvu.edu.in>
2026-04-07 13:24:26 +0530

    Update prog4.sh

--- prog4.sh
+++ prog4.sh
@@ -34,9 +34,4 @@ echo "Invalid choice!"
 fi
 
 echo ""
-done
-
-
-
-^G Get Help            ^O WriteOut            ^R Read File           ^Y Prev Pg             ^K Cut Text            ^C Cur Pos             
-^X Exit                ^J Justify             ^W Where is            ^V Next Pg             ^U UnCut Text          ^T To Spell            
+done 
